017f220d4816125e002d45f15671c10b395cb57d
Pavel Smejkal <smejkal.pa@gmail.com>
2017-08-04 14:19:11 +0200

    Add interface working as an abstract class through macros

---
+++ fruiton/IAbstractClass.hx
@@ -0,0 +1,8 @@
+package fruiton;
+
+/**
+ * Interface enabling classes to be abstract in C++/Java sense.
+ * Class with abstract method (without body) and public constructor will be shown as compiler error.
+ */
+@:autoBuild(fruiton.AbstractClassBuilder.build())
+interface IAbstractClass {}
