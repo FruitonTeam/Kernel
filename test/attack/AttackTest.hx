7e550c6b077525e166cb5bfad6882b3af7e802f0
Pavel Smejkal <smejkal.pa@gmail.com>
2017-08-08 17:49:22 +0200

    Add attack action tests

--- test/attack/AttackTest.hx
+++ test/attack/AttackTest.hx
@@ -25,24 +25,122 @@ class AttackTest {
      * Factory method for unified and simple kernel creation
      * @return Kernel which is initialized
      */
-    function makeKernel():Kernel {
-		var p1:Player = new Player(1);
-		var p2:Player = new Player(2);
+    function makeKernel(kill:Bool):Kernel {
+		var p1:Player = new Player(0);
+		var p2:Player = new Player(1);
+
+        var hp:Int = 10;
+        var dmg:Int = kill ? hp : hp - 1;
 
         var moveGenerators:MoveGenerators = new MoveGenerators();
         moveGenerators.push(new MoveGenerator(new LineTargetPattern(new Vector2(0, 1), -1, 1)));
         moveGenerators.push(new MoveGenerator(new LineTargetPattern(new Vector2(1, 0), -1, 1)));
 
         var attackGenerators:AttackGenerators = new AttackGenerators();
-        attackGenerators.push(new AttackGenerator(new RangeTargetPattern(Vector2.ZERO, 0, 1), 5));
-        attackGenerators.push(new AttackGenerator(new LineTargetPattern(new Vector2(1, 0), -1, 1), 5));
+        attackGenerators.push(new AttackGenerator(new RangeTargetPattern(Vector2.ZERO, 0, 1), dmg));
+        attackGenerators.push(new AttackGenerator(new LineTargetPattern(new Vector2(1, 0), -1, 1), dmg));
 
-        var fruiton:Fruiton = new Fruiton(1, new Vector2(0, 1), p1, 10, moveGenerators, attackGenerators);
-		return new Kernel(p1, p2, [fruiton]);
+        var fruiton:Fruiton = new Fruiton(1, new Vector2(0, 0), p1, hp, moveGenerators, attackGenerators);
+        var fruiton2:Fruiton = new Fruiton(2, new Vector2(0, 1), p2, hp, moveGenerators, attackGenerators);
+		return new Kernel(p1, p2, [fruiton, fruiton2]);
 	}
 
     @Test
-    public function f() {
+    public function getAllValidActions_fruitonsInRange_returnsOneAttackAction() {
+        Sys.println("=== running getAllValidActions_fruitonsInRange_returnsOneAttackAction");
 
+		var k:Kernel = makeKernel(true);
+		var actions:IKernel.Actions = k.getAllValidActions();
+
+		var hasAttackAction:Bool = false;
+		for (a in actions) {
+			if (Std.is(a, AttackAction)) {
+                Assert.isFalse(hasAttackAction);
+				hasAttackAction = true;
+			}
+		}
+		Assert.isTrue(hasAttackAction);
     }
+
+    @Test
+	public function performAction_validAttackAction_returnsMatchingEvent() {
+		Sys.println("=== running performAction_validAttackAction_returnsMatchingEvent");
+
+		var k:Kernel = makeKernel(true);
+		var actions:IKernel.Actions = k.getAllValidActions();
+
+		var action:AttackAction = null;
+		for (a in actions) {
+			if (Std.is(a, AttackAction)) {
+				action = cast(a, AttackAction);
+				break;
+			}
+		}
+
+		var result:IKernel.Events = k.performAction(action);
+		var event:AttackEvent = null;
+		for (e in result) {
+			if (Std.is(e, AttackEvent)) {
+				event = cast(e, AttackEvent);
+				break;
+			}
+		}
+
+		Assert.isTrue(event != null);
+		Assert.isTrue(action.actionContext.source == event.source);
+		Assert.isTrue(action.actionContext.target == event.target);
+        Assert.isTrue(action.actionContext.damage == event.damage);
+	}
+
+    @Test
+	public function performAction_attackKill_returnsDeathEvent() {
+		Sys.println("=== running performAction_attackKill_returnsDeathEvent");
+
+		var k:Kernel = makeKernel(true);
+		var actions:IKernel.Actions = k.getAllValidActions();
+
+		var action:AttackAction = null;
+		for (a in actions) {
+			if (Std.is(a, AttackAction)) {
+				action = cast(a, AttackAction);
+				break;
+			}
+		}
+
+		var result:IKernel.Events = k.performAction(action);
+		var event:DeathEvent = null;
+		for (e in result) {
+			if (Std.is(e, DeathEvent)) {
+				event = cast(e, DeathEvent);
+				break;
+			}
+		}
+
+		Assert.isTrue(event != null);
+		Assert.isTrue(action.actionContext.target == event.target);
+	}
+
+    @Test
+	public function performAction_attackNoKill_lowersFruitonHealth() {
+		Sys.println("=== running performAction_attackNoKill_lowersFruitonHealth");
+
+		var k:Kernel = makeKernel(false);
+		var actions:IKernel.Actions = k.getAllValidActions();
+
+		var action:AttackAction = null;
+		for (a in actions) {
+			if (Std.is(a, AttackAction)) {
+				action = cast(a, AttackAction);
+				break;
+			}
+		}
+
+        var hpBefore:Int = k.currentState.field.get(action.actionContext.target).fruiton.hp;
+
+		k.performAction(action);
+
+        var hpAfter:Int = k.currentState.field.get(action.actionContext.target).fruiton.hp;
+
+		Assert.isTrue(hpBefore - hpAfter == action.actionContext.damage);
+	}
 }
