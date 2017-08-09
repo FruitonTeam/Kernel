bdfec06169370b2d59608012b0d6c1494774c476
Pavel Smejkal <smejkal.pa@gmail.com>
2017-08-09 09:58:51 +0200

    Add javadoc to Hlinq

--- fruiton/dataStructures/Hlinq.hx
+++ fruiton/dataStructures/Hlinq.hx
@@ -2,6 +2,12 @@ package fruiton.dataStructures;
 
 class Hlinq {
 
+    /**
+     * Finds and returns first item of given type, null of non found
+     * @param items - List to search in
+     * @param type - Type to search in `items`
+     * @return First item of `type` in `items`, null otherwise
+     */
     public static function firstOfTypeOrNull<T, U>(items:Iterable<T>, type:Class<U>):U {
         for (item in items) {
             if (Std.is(item, type)) {
@@ -11,6 +17,12 @@ class Hlinq {
         return null;
     }
 
+    /**
+     * Finds and returns single item of given type, null if none or more than one found
+     * @param items - List to search in
+     * @param type - Type to search in `items`
+     * @return Single item of `type` in `items`, null otherwise
+     */
     public static function singleOfTypeOrNull<T, U>(items:Iterable<T>, type:Class<U>):U {
         var result:U = null;
 
