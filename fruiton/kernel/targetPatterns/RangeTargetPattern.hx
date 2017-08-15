3969864aa8b41690a96d3de6940fd844bca1c7d9
Pavel Smejkal <smejkal.pa@gmail.com>
2017-08-15 11:28:18 +0200

    Improve RangeTargetPattern now able to produce rings

--- fruiton/kernel/targetPatterns/RangeTargetPattern.hx
+++ fruiton/kernel/targetPatterns/RangeTargetPattern.hx
@@ -2,6 +2,9 @@ package fruiton.kernel.targetPatterns;
 
 import fruiton.dataStructures.Vector2;
 
+/**
+ * Produces targets in range (ring) around given `origin` from `min` to `max` (both inclusive)
+ */
 class RangeTargetPattern extends TargetPattern {
 
     public function new (vector:Vector2, min:Int, max:Int) {
@@ -13,6 +16,10 @@ class RangeTargetPattern extends TargetPattern {
 
         for (x in (-max)...(max + 1)) {
             for (y in (-max)...(max + 1)) {
+                if (Math.abs(x) < min &&
+                    Math.abs(y) < min) {
+                    continue;
+                }
                 targets.push(origin + new Vector2(x, y));
             }
         }
