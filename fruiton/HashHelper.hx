84bc95b289100ebe34edde8991b57dd6aade9d4c
Jozef Lelic <joseph.lelic@gmail.com>
2017-10-09 11:11:20 +0200

    Add basic unit test for hash functions

--- fruiton/HashHelper.hx
+++ fruiton/HashHelper.hx
@@ -6,8 +6,11 @@ class HashHelper {
 
     public static var SUM_FUNCTION = function (num: Int, total: Int) return total += num;
 
-    public static function getPrime(i: Int) :Int {
-        return PRIMES[i % PRIMES.length];
+    /**
+     * Return a prime from primes.txt file, if the index is too high it loops back
+     */
+    public static function getPrime(index: Int) :Int {
+        return PRIMES[index % PRIMES.length];
     }
 
     public static function hashString(string: String): Int {
@@ -18,6 +21,9 @@ class HashHelper {
         ) + getPrime(8);
     }
 
+    /**
+     * Creates order dependent hash of iterable object
+     */
     public static function hashIterable(iterable: Iterable<IHashable>, indexStart: Int, indexDiff: Int): Int {
         var result: Int = 0;
         var primeIndex = indexStart;
---
+++ test/hashFunctions/HashFunctionsTest.hx
@@ -0,0 +1,67 @@
+package test.hashFunctions;
+
+import sys.io.File;
+import fruiton.dataStructures.Vector2;
+import fruiton.kernel.targetPatterns.LineTargetPattern;
+import fruiton.kernel.targetPatterns.RangeTargetPattern;
+import fruiton.kernel.AttackGenerator;
+import fruiton.kernel.Player;
+import fruiton.kernel.MoveGenerator;
+import fruiton.kernel.Fruiton;
+import fruiton.kernel.TurnState;
+import fruiton.kernel.GameState;
+import fruiton.HashHelper;
+import massive.munit.Assert;
+
+class HashFunctionsTest {
+
+    public function new() {}
+
+    @BeforeClass
+    public function beforeClass() {
+        Sys.println("======================");
+        Sys.println("Hash functions tests");
+        Sys.println("======================");
+    }
+
+    @Test
+    public function hashFunctions_areDefined() {
+        Sys.println("=== running hashFunctions_areDefined");
+
+        var vector2: Vector2 = new Vector2(0, 0);
+        var lineTargetPattern: LineTargetPattern = new LineTargetPattern(vector2, 1, 0);
+        var attackGenerator: AttackGenerator = new AttackGenerator(lineTargetPattern, 1);
+        var player: Player = new Player(0);
+        var moveGenerator: MoveGenerator = new MoveGenerator(lineTargetPattern);
+        var fruiton: Fruiton = new Fruiton(0, vector2, player, 2, "model1", [], [], 0);
+        var gameState: GameState = new GameState([player], 0, [fruiton]);
+
+        Assert.isType(vector2.getHashCode(), Int);
+        Assert.isType(lineTargetPattern.getHashCode(), Int);
+        Assert.isType(new RangeTargetPattern(vector2, 1, 0).getHashCode(), Int);
+        Assert.isType(attackGenerator.getHashCode(), Int);
+        Assert.isType(player.getHashCode(), Int);
+        Assert.isType(moveGenerator.getHashCode(), Int);
+        Assert.isType(new TurnState().getHashCode(), Int);
+        Assert.isType(fruiton.getHashCode(), Int);
+        Assert.isType(gameState.getHashCode(), Int);
+        Assert.isType(HashHelper.hashString("aaa"), Int);
+        Assert.isType(HashHelper.hashIterable([player], 0, 1), Int);
+
+    }
+
+    @Test
+    public function hashIterable_isOrderDependent() {
+        Sys.println("=== running hashIterable_isOrderDependent");
+
+        var player1: Player = new Player(0);
+        var player2: Player = new Player(1);
+
+        Assert.areNotEqual(player1.getHashCode(), player2.getHashCode());
+
+        Assert.areNotEqual(
+            HashHelper.hashIterable([player1, player2], 0, 1),
+            HashHelper.hashIterable([player2, player1], 0, 1)
+            );
+    }
+}
