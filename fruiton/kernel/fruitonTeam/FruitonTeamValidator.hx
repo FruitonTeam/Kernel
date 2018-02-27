80f0a0a64ce7128d61a1e9c9966adbc567c8b39b
Adam Hornáček <adam.hornacek@icloud.com>
2018-02-27 20:53:35 +0100

    SWP-459 add method to validate fruiton positions
    Initial commit

--- fruiton/kernel/fruitonTeam/FruitonTeamValidator.hx
+++ fruiton/kernel/fruitonTeam/FruitonTeamValidator.hx
@@ -1,10 +1,32 @@
 package fruiton.kernel.fruitonTeam;
 
+import fruiton.dataStructures.collections.ArrayOfEquitables;
+import fruiton.dataStructures.Point;
 import fruiton.fruitDb.FruitonDatabase;
+import fruiton.kernel.exceptions.InvalidArgumentException;
 
 class FruitonTeamValidator {
 
     static var REQUIRED_COUNTS(default, never):Array<Int> = [1, 4, 5];
+
+    static var VALID_KING_POSITIONS(default, never):ArrayOfEquitables<Point> = [
+        new Point(4, 0), new Point(4, GameState.WIDTH)
+    ];
+
+    static var VALID_MAJOR_POSITIONS(default, never):ArrayOfEquitables<Point> = [
+        new Point(2, 0), new Point(2, GameState.WIDTH),
+        new Point(3, 0), new Point(3, GameState.WIDTH),
+        new Point(5, 0), new Point(5, GameState.WIDTH),
+        new Point(6, 0), new Point(6, GameState.WIDTH)
+    ];
+
+    static var VALID_MINOR_POSITIONS(default, never):ArrayOfEquitables<Point> = [
+        new Point(2, 1), new Point(2, GameState.WIDTH - 1),
+        new Point(3, 1), new Point(3, GameState.WIDTH - 1),
+        new Point(4, 1), new Point(4, GameState.WIDTH - 1),
+        new Point(5, 1), new Point(5, GameState.WIDTH - 1),
+        new Point(6, 1), new Point(6, GameState.WIDTH - 1)
+    ];
 
     /**
      * @param fruitonIds Array of ids of the controlled Fruiton Team.
@@ -28,5 +50,39 @@ class FruitonTeamValidator {
             }
         }
         return new ValidationResult(valid, complete);
+    }
+
+    public static function haveValidPositions(fruitons:Array<Fruiton>):Bool {
+
+        var usedPositions:ArrayOfEquitables<Point> = [];
+
+        for (fruiton in fruitons) {
+            if (!hasValidPosition(fruiton)) {
+                return false;
+            }
+            if (usedPositions.contains(fruiton.position)) {
+                return false;
+            }
+            usedPositions.push(fruiton.position);
+        }
+
+        return true;
+    }
+
+    static function hasValidPosition(fruiton:Fruiton):Bool {
+        switch (fruiton.type) {
+            case Fruiton.KING_TYPE: {
+                return VALID_KING_POSITIONS.contains(fruiton.position);
+            }
+            case Fruiton.MAJOR_TYPE: {
+                return VALID_MAJOR_POSITIONS.contains(fruiton.position);
+            }
+            case Fruiton.MINOR_TYPE: {
+                return VALID_MINOR_POSITIONS.contains(fruiton.position);
+            }
+            default: {
+                throw new InvalidArgumentException("Unknown fruiton type " + fruiton.type);
+            }
+        }
     }
 }
